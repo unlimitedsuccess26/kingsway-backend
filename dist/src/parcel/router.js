3cd0ca323d46ef6030956c07b76d4765b621e7ff
unlimitedsuccess26 <chantaramalee6@gmail.com>
2024-12-19 09:19:12 +0100

    added auth

--- dist/src/parcel/router.js
+++ dist/src/parcel/router.js
@@ -5,6 +5,7 @@ const express_1 = require("express");
 const controller_1 = require("./controller");
 const utils_1 = require("../utils");
 const validator_1 = require("./validator");
+const isAuth_1 = require("../middleware/isAuth");
 exports.ParcelRouter = (0, express_1.Router)();
 //create Parcel
-exports.ParcelRouter.post("/create/parcel", [validator_1.createParcelValidator.createParcel], (0, utils_1.wrapAsync)(controller_1.createParcelController.createParcel));
+exports.ParcelRouter.post("/create/parcel", [isAuth_1.isAuth, validator_1.createParcelValidator.createParcel], (0, utils_1.wrapAsync)(controller_1.createParcelController.createParcel));
--- src/parcel/router.ts
+++ src/parcel/router.ts
@@ -3,13 +3,14 @@ import { Router } from "express";
 import { createParcelController } from "./controller";
 import { wrapAsync } from "../utils";
 import { createParcelValidator } from "./validator";
+import { isAuth } from "../middleware/isAuth";
 
 export const ParcelRouter = Router();
 
 //create Parcel
 ParcelRouter.post(
   "/create/parcel",
-  [ createParcelValidator.createParcel],
+  [isAuth, createParcelValidator.createParcel],
   wrapAsync(createParcelController.createParcel)
 );
 
